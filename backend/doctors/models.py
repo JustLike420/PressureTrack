af40829dc49a708fba3f56bf2e14de75ea7764de
Vladimir <44843816+JustLike420@users.noreply.github.com>
2023-04-08 12:37:57 +0300

    Backend | create models for doctor (#3)
    * base configuration
    
    * fix docker
    
    * create models for doctor

--- backend/doctors/models.py
+++ backend/doctors/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    snils = models.CharField(max_length=14, unique=True)
+
+    class Meta:
+        db_table = 'doctor'
+
+
+class PatientDoctor(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='patient_doctor')
+    patient = models.ForeignKey(
+        'patients.Patient',
+        on_delete=models.CASCADE,
+        related_name='doctors_patient'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+
+class Treatment(models.Model):
+    patient = models.ForeignKey(
+        'patients.Patient',
+        on_delete=models.CASCADE,
+        related_name='patient_treatment',
+        default=1
+    )
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'treatment'
--- backend/patients/models.py
+++ backend/patients/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = 'patient'
