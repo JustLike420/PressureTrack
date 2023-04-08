90818ddc0f5b3ee31ea6e5380ddf47b34d9559ef
Vladimir <justlikevova@gmail.com>
2023-04-08 22:56:03 +0300

    Revert "add BaseUser model"
    This reverts commit 366256a9d717fb37ae30fadeb2a35418e71baee2.

--- backend/admins/models.py
+++ backend/admins/models.py
@@ -1,15 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 
-
-class BaseUser(models.Model):
-    id = models.AutoField(User, primary_key=True)
-    snils = models.CharField(max_length=14, unique=True)
-    first_name = models.TextField()
-    second_name = models.TextField()
-    last_name = models.TextField(null=True)
-    age = models.IntegerField()
-    password = models.BinaryField()
-
-    class Meta:
-        abstract = True
+# Create your models here.
