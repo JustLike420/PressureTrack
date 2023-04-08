366256a9d717fb37ae30fadeb2a35418e71baee2
DimaRFF <dimarff.pro@gmail.com>
2023-04-08 14:32:12 +0300

    add BaseUser model

--- backend/admins/models.py
+++ backend/admins/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class BaseUser(models.Model):
+    id = models.AutoField(User, primary_key=True)
+    snils = models.CharField(max_length=14, unique=True)
+    first_name = models.TextField()
+    second_name = models.TextField()
+    last_name = models.TextField(null=True)
+    age = models.IntegerField()
+    password = models.BinaryField()
+
+    class Meta:
+        abstract = True
