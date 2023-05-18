fe5a891d28077f92b32a9bed31d6b51c1b8d0ced
Vladimir <justlikevova@gmail.com>
2023-05-18 23:38:49 +0300

    - show user profile

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
 
-from accounts.serializers import CreatePatient
+from accounts.models import CustomUser
+from accounts.serializers import CreatePatient, CustomUserSerializer
 from patients.models import Patient
 
 
 class CreatePatientView(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = CreatePatient
+
+
+class ProfileView(viewsets.ModelViewSet):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
