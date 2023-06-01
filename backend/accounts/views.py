c06514d44ef52c011fa76944ccc53d10df506d2d
DimaRFF <71233113+tvoy-papa@users.noreply.github.com>
2023-06-01 15:32:44 +0300

    Added status code 200, 400, 401, 500

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from accounts.serializers import CreatePatient, CustomUserSerializer
 from patients.models import Patient
+from rest_framework.exceptions import NotAuthenticated, server_error, NotFound
 
 
 class CreatePatientView(viewsets.ModelViewSet):
@@ -16,9 +17,18 @@ class CreatePatientView(viewsets.ModelViewSet):
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
 
-    @swagger_auto_schema(responses={
-        200: openapi.Response('response description', CustomUserSerializer)
-    })
+    @swagger_auto_schema(
+        responses={
+            200: openapi.Response('response description', CustomUserSerializer),
+            401: openapi.Response('Unauthorized'),
+            500: openapi.Response('Internal Server Error'),
+        }
+    )
     def get(self, request):
-        serializer = CustomUserSerializer(self.request.user)
-        return Response(serializer.data)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated()
+        try:
+            serializer = CustomUserSerializer(self.request.user)
+            return Response(serializer.data)
+        except Exception as e:
+            return server_error(request)
